e360a43a78af1311d6c4b12312ad761595040b3a
selnem <tjdghks0130@naver.com>
2024-07-08 14:04:12 +0900

    Title: 격자 모양 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240708/격자 모양 출력하기/print-grid-shape.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int m,n,x,y;
+    scanf("%d %d",&n,&m);
+    int arr[100][100];
+    for (int i=0;i<m;i++){
+        scanf("%d %d",&x,&y);
+        arr[x][y]=x*y;
+    }
+    for (int j=1;j<=n;j++){
+        for(int k=1;k<=n;k++){
+            printf("%d ",arr[j][k]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
