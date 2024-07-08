b8f01ba95a352e2f1fda2f8c8529e68b66f10a92
selnem <tjdghks0130@naver.com>
2024-07-08 14:08:09 +0900

    Title: 격자 모양 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240708/격자 모양 출력하기/print-grid-shape.c
+++ 240708/격자 모양 출력하기/print-grid-shape.c
@@ -3,7 +3,7 @@
 int main() {
     int m,n,x,y;
     scanf("%d %d",&n,&m);
-    int arr[4*m+1][4*n+1];
+    int arr[4*n+1][4*m+1];
     for (int i=0;i<m;i++){
         scanf("%d %d",&x,&y);
         arr[x][y]=x*y;
